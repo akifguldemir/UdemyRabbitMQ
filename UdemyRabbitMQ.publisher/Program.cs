ae4e2f5ec066762814a3231d0eef7ebba3323e48
Akif Güldemir <akifguldemir@gmail.com>
2025-02-16 19:57:33 +0300

    RabbitMQ başarılı

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+class Program
+{
+    static async Task Main()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost"
+        };
+
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(
+            queue: "task_queue", // Kuyruk adı
+            durable: true, // Kuyruğun kalıcı olup olmayacağı
+            exclusive: false, // Sadece bu bağlantıdan erişilebilir olup olmayacağı
+            autoDelete: false, // Son tüketici bağlandığında kuyruğun silinip silinmeyeceği
+            arguments: null // Kuyruk argümanları
+        );
+
+        string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync(
+            exchange: "",
+            routingKey: "task_queue",
+            mandatory: false,
+            basicProperties: new BasicProperties
+            {
+                DeliveryMode = (DeliveryModes)2 // 2 = Kalıcı mesaj
+            },
+            body: body
+        );
+
+        Console.WriteLine(" [x] Sent {0}", message);
+    }
+}
--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Program
+{
+    static async Task Main()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost"
+        };
+
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+
+        //Senaryoya göre kuyruk burada oluşturulabilir
+
+        //await channel.QueueDeclareAsync(
+        //    queue: "task_queue", // Kuyruk adı
+        //    durable: true, // Kuyruğun kalıcı olup olmayacağı
+        //    exclusive: false, // Sadece bu bağlantıdan erişilebilir olup olmayacağı
+        //    autoDelete: false, // Son tüketici bağlandığında kuyruğun silinip silinmeyeceği
+        //    arguments: null // Kuyruk argümanları
+        //); 
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        await channel.BasicConsumeAsync(
+            "task_queue", // Kuyruk adı
+            true, // Mesajın işlendiğini doğrulamak için false
+            consumer // Tüketici
+        );
+
+        consumer.ReceivedAsync += (object sender, BasicDeliverEventArgs eventArgs) =>
+        {
+            var body = eventArgs.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+            return Task.CompletedTask;
+        };
+
+        Console.ReadLine();
+    }
+}
