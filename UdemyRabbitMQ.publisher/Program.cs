730c16e8b3d5f77193ce5a1e399ae1b76989ed18
akifguldemir <akif.guldemir@ozyer.com>
2025-02-18 23:07:08 +0300

    Fanout exchange tye added

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -8,7 +8,48 @@ class Program
     static async Task Main()
     {
 
-        // No Exchange, No Queue, No Binding
+        //// No Exchange, No Queue, No Binding
+        //var factory = new ConnectionFactory
+        //{
+        //    HostName = "localhost"
+        //};
+
+        //await using var connection = await factory.CreateConnectionAsync();
+        //await using var channel = await connection.CreateChannelAsync();
+
+        //await channel.QueueDeclareAsync(
+        //    queue: "task_queue", // Kuyruk adı
+        //    durable: true, // Kuyruğun kalıcı olup olmayacağı
+        //    exclusive: false, // Sadece bu bağlantıdan erişilebilir olup olmayacağı
+        //    autoDelete: false, // Son tüketici bağlandığında kuyruğun silinip silinmeyeceği
+        //    arguments: null // Kuyruk argümanları
+        //);
+
+        //Enumerable.Range(1, 50).ToList().ForEach(async x =>
+        //{
+        //    string message = $"Message {x}";
+        //    var body = Encoding.UTF8.GetBytes(message);
+
+        //    await channel.BasicPublishAsync(
+        //        exchange: "",
+        //        routingKey: "task_queue",
+        //        mandatory: false,
+        //        basicProperties: new BasicProperties
+        //        {
+        //            DeliveryMode = (DeliveryModes)2 // 2 = Kalıcı mesaj
+        //        },
+        //        body: body
+        //    );
+
+        //    Console.WriteLine($"Mesaj gönderildi {message}");
+        //});
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        //Fanout Exchange.
+        //Filtreleme yapmaksızın tüm kuyruklara gönderir..hava durumu senaryosu
+
         var factory = new ConnectionFactory
         {
             HostName = "localhost"
@@ -17,22 +58,19 @@ class Program
         await using var connection = await factory.CreateConnectionAsync();
         await using var channel = await connection.CreateChannelAsync();
 
-        await channel.QueueDeclareAsync(
-            queue: "task_queue", // Kuyruk adı
-            durable: true, // Kuyruğun kalıcı olup olmayacağı
-            exclusive: false, // Sadece bu bağlantıdan erişilebilir olup olmayacağı
-            autoDelete: false, // Son tüketici bağlandığında kuyruğun silinip silinmeyeceği
-            arguments: null // Kuyruk argümanları
-        );
+        await channel.ExchangeDeclareAsync("logs-fanout",
+            durable: false, // fiziksel olarak kaydolur
+            type: ExchangeType.Fanout
+            );
 
         Enumerable.Range(1, 50).ToList().ForEach(async x =>
         {
-            string message = $"Message {x}";
+            string message = $"log {x}";
             var body = Encoding.UTF8.GetBytes(message);
 
             await channel.BasicPublishAsync(
-                exchange: "",
-                routingKey: "task_queue",
+                exchange: "logs-fanout",
+                routingKey: "", // filtreleme olmadığı için route key yok
                 mandatory: false,
                 basicProperties: new BasicProperties
                 {
@@ -44,6 +82,8 @@ class Program
             Console.WriteLine($"Mesaj gönderildi {message}");
         });
 
-        
+
+
+
     }
 }
--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -8,6 +8,56 @@ class Program
 {
     static async Task Main()
     {
+
+        ////No Exchange
+        //var factory = new ConnectionFactory
+        //{
+        //    HostName = "localhost"
+        //};
+
+        //await using var connection = await factory.CreateConnectionAsync();
+        //await using var channel = await connection.CreateChannelAsync();
+
+        ////Senaryoya göre kuyruk burada oluşturulabilir
+
+        ////await channel.QueueDeclareAsync(
+        ////    queue: "task_queue", // Kuyruk adı
+        ////    durable: true, // Kuyruğun kalıcı olup olmayacağı
+        ////    exclusive: false, // Sadece bu bağlantıdan erişilebilir olup olmayacağı
+        ////    autoDelete: false, // Son tüketici bağlandığında kuyruğun silinip silinmeyeceği
+        ////    arguments: null // Kuyruk argümanları
+        ////); 
+
+        //await channel.BasicQosAsync(
+        //    0,
+        //    6, //Gödnerilecek sayı
+        //    false // her birine 6 tane gönderir. true 6 yıl böler
+        //    );
+        //var consumer = new AsyncEventingBasicConsumer(channel);
+
+        //await channel.BasicConsumeAsync(
+        //    "task_queue", // Kuyruk adı
+        //    false, // Mesajın işlendiğini doğrulamak için false..BasicAckAsync ile bilgi gelene kadar mesajı silmez
+        //    consumer // Tüketici
+        //);
+
+        //consumer.ReceivedAsync += (object sender, BasicDeliverEventArgs eventArgs) =>
+        //{
+        //    var body = eventArgs.Body.ToArray();
+        //    var message = Encoding.UTF8.GetString(body);
+
+        //    Thread.Sleep(1500);
+        //    Console.WriteLine(" [x] Received {0}", message);
+        //    channel.BasicAckAsync(eventArgs.DeliveryTag, false);
+
+        //    return Task.CompletedTask;
+        //};
+
+        //Console.ReadLine();
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        //Fanout
         var factory = new ConnectionFactory
         {
             HostName = "localhost"
@@ -16,28 +66,28 @@ class Program
         await using var connection = await factory.CreateConnectionAsync();
         await using var channel = await connection.CreateChannelAsync();
 
-        //Senaryoya göre kuyruk burada oluşturulabilir
+        var queueDeclareOk = await channel.QueueDeclareAsync();
+        var randomQueueName = queueDeclareOk.QueueName;
 
-        //await channel.QueueDeclareAsync(
-        //    queue: "task_queue", // Kuyruk adı
-        //    durable: true, // Kuyruğun kalıcı olup olmayacağı
-        //    exclusive: false, // Sadece bu bağlantıdan erişilebilir olup olmayacağı
-        //    autoDelete: false, // Son tüketici bağlandığında kuyruğun silinip silinmeyeceği
-        //    arguments: null // Kuyruk argümanları
-        //); 
+        await channel.QueueBindAsync(randomQueueName, "logs-fanout", "", null); // uygulama çalışınca otomatik kuyruk oluşur, kapanınca silinir.
+        //Eğer kuyruk kalsın isersek declare edebiliriz.
 
         await channel.BasicQosAsync(
             0,
-            6, //Gödnerilecek sayı
+            1, //Gödnerilecek sayı
             false // her birine 6 tane gönderir. true 6 yıl böler
             );
         var consumer = new AsyncEventingBasicConsumer(channel);
 
+        await channel.QueueDeclareAsync(randomQueueName, true, false, false);//hazır mesaj alırken gelen mesajlar kaybolmasın diye kuyruk olusturduk. server down olursa kuyruktaki mesajları alabiliriz.
+
         await channel.BasicConsumeAsync(
-            "task_queue", // Kuyruk adı
+            randomQueueName, // Kuyruk adı
             false, // Mesajın işlendiğini doğrulamak için false..BasicAckAsync ile bilgi gelene kadar mesajı silmez
             consumer // Tüketici
         );
+
+        Console.WriteLine("Loglar dinleniyor...");
 
         consumer.ReceivedAsync += (object sender, BasicDeliverEventArgs eventArgs) =>
         {
