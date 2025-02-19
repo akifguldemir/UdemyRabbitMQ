57ad8be694f7b8c5a4fc2e3340ae33d752d9efd8
akifguldemir <akif.guldemir@ozyer.com>
2025-02-19 09:03:42 +0300

    Started to code direct exchange

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -3,6 +3,14 @@ using System.Text;
 using System.Threading.Tasks;
 using RabbitMQ.Client;
 
+public enum LogNames
+{
+    Critical=1,
+    Error=2,
+    Warning=3,
+    Info4
+}
+
 class Program
 {
     static async Task Main()
@@ -50,6 +58,45 @@ class Program
         //Fanout Exchange.
         //Filtreleme yapmaksızın tüm kuyruklara gönderir..hava durumu senaryosu
 
+        //var factory = new ConnectionFactory
+        //{
+        //    HostName = "localhost"
+        //};
+
+        //await using var connection = await factory.CreateConnectionAsync();
+        //await using var channel = await connection.CreateChannelAsync();
+
+        //await channel.ExchangeDeclareAsync("logs-fanout",
+        //    durable: false, // fiziksel olarak kaydolur
+        //    type: ExchangeType.Fanout
+        //    );
+
+        //Enumerable.Range(1, 50).ToList().ForEach(async x =>
+        //{
+        //    string message = $"log {x}";
+        //    var body = Encoding.UTF8.GetBytes(message);
+
+        //    await channel.BasicPublishAsync(
+        //        exchange: "logs-fanout",
+        //        routingKey: "", // filtreleme olmadığı için route key yok
+        //        mandatory: false,
+        //        basicProperties: new BasicProperties
+        //        {
+        //            DeliveryMode = (DeliveryModes)2 // 2 = Kalıcı mesaj
+        //        },
+        //        body: body
+        //    );
+
+        //    Console.WriteLine($"Mesaj gönderildi {message}");
+        //});
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        //Direct Exchange.
+        //mesajı direk ilgili kuyruğa gönderir. rastgele log seviyeleri sahip loglar var diyelim.
+        //Kuyruk oluşturacaz ve logu gerekn kuyruğa gönderecez
+
         var factory = new ConnectionFactory
         {
             HostName = "localhost"
@@ -58,10 +105,15 @@ class Program
         await using var connection = await factory.CreateConnectionAsync();
         await using var channel = await connection.CreateChannelAsync();
 
-        await channel.ExchangeDeclareAsync("logs-fanout",
+        await channel.ExchangeDeclareAsync("logs-direct",
             durable: false, // fiziksel olarak kaydolur
-            type: ExchangeType.Fanout
+            type: ExchangeType.Direct
             );
+
+        foreach (var item in Enum.GetNames(typeof(LogNames)).ToList())
+        {
+            var queueName = $"direct-queue-{item}";
+        }
 
         Enumerable.Range(1, 50).ToList().ForEach(async x =>
         {
